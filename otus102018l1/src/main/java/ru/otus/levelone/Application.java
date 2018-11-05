b4ccb16bd86a2303587ef99aa90e13ec397cbcd0
AndreiT89 <toporkov1989@yandex.ru>
2018-11-05 22:37:06 +0300

    guava class usage added

--- otus102018l1/src/main/java/ru/otus/levelone/Application.java
+++ otus102018l1/src/main/java/ru/otus/levelone/Application.java
@@ -1,7 +1,15 @@
 package ru.otus.levelone;
+import java.util.Arrays;
+import com.google.common.base.Joiner;
 
 public class Application {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        Application app = new Application();
+        app.joinString();
+    }
+    private void joinString(){
+        System.out.println(Joiner.on(",")
+                .skipNulls()
+                .join(Arrays.asList("Hello","World!")));
     }
 }
