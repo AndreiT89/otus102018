339174ddc4febf47faa4e0ccbeb3a7cc8a0d46a9
atoporkov <atoporkov@gmcs.ru>
2019-02-26 11:14:49 +0300

    Annotations homework fix with reflection

--- annotationshomework/src/main/java/ru/annotations/Main.java
+++ annotationshomework/src/main/java/ru/annotations/Main.java
@@ -3,20 +3,21 @@ package ru.annotations;
 import ru.annotations.test.FirstTest;
 import ru.annotations.test.SecondTest;
 import ru.annotations.test.ThirdTest;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import ru.annotations.test.TestLauncher;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<Object> list = new ArrayList<>();
-        FirstTest ft = new FirstTest();
-        SecondTest st = new SecondTest();
-        ThirdTest tt = new ThirdTest();
-        list.add(ft);
-        list.add(st);
-        list.add(tt);
-        TestLauncher.launch(list);
+        List<Class> list = new ArrayList<>();
+        list.add(FirstTest.class);
+        list.add(SecondTest.class);
+        list.add(ThirdTest.class);
+        for (Class cls : list) {
+            TestLauncher.launch(cls);
+        }
     }
 
 
--- annotationshomework/src/main/java/ru/annotations/test/TestLauncher.java
+++ annotationshomework/src/main/java/ru/annotations/test/TestLauncher.java
@@ -9,46 +9,42 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 public class TestLauncher {
-    public static void launch(List<?> objs) throws Exception {
+    public static void launch(Class cls) throws Exception {
+        Object obj = cls.getDeclaredConstructor().newInstance();
+        Method[] clsMeth = obj.getClass().getDeclaredMethods();
+        for (Method meth : clsMeth) {
+            if (meth.getAnnotation(Before.class) != null) {
+                try {
+                    meth.invoke(obj);
+                } catch (IllegalAccessException ilEx) {
+                    throw new Exception("test preparation failed");
+                } catch (InvocationTargetException invExc) {
+                    throw new Exception("test preparation failed");
+                }
 
-        for (Object obj : objs) {
-            Method[] clsMeth = obj.getClass().getDeclaredMethods();
-            for (Method meth : clsMeth) {
-                if (meth.getAnnotation(Before.class) != null) {
-                    try {
-                        meth.invoke(obj);
-                    } catch (IllegalAccessException ilEx) {
-                        throw new Exception("test preparation failed");
-                    } catch (InvocationTargetException invExc) {
-                        throw new Exception("test preparation failed");
-                    }
-
+            }
+        }
+        for (Method meth : clsMeth) {
+            if (meth.getAnnotation(Test.class) != null) {
+                try {
+                    meth.invoke(obj);
+                } catch (IllegalAccessException ilEx) {
+                    throw new Exception("test run failed");
+                } catch (InvocationTargetException invExc) {
+                    throw new Exception("test run failed");
                 }
             }
-            for (Method meth : clsMeth) {
-                if (meth.getAnnotation(Test.class) != null) {
-                    try {
-                        meth.invoke(obj);
-                    } catch (IllegalAccessException ilEx) {
-                        throw new Exception("test run failed");
-                    } catch (InvocationTargetException invExc) {
-                        throw new Exception("test run failed");
-                    }
+        }
+        for (Method meth : clsMeth) {
+            if (meth.getAnnotation(After.class) != null) {
+                try {
+                    meth.invoke(obj);
+                } catch (IllegalAccessException ilEx) {
+                    throw new Exception("test closing failed");
+                } catch (InvocationTargetException invExc) {
+                    throw new Exception("test closing failed");
                 }
             }
-            for (Method meth : clsMeth) {
-                if (meth.getAnnotation(After.class) != null) {
-                    try {
-                        meth.invoke(obj);
-                    } catch (IllegalAccessException ilEx) {
-                        throw new Exception("test closing failed");
-                    } catch (InvocationTargetException invExc) {
-                        throw new Exception("test closing failed");
-                    }
-                }
-            }
-
-
         }
     }
 }
