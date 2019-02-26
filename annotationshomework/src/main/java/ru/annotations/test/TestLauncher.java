192aee6dc465a94c64afe64e42eb4da19086b9c6
atoporkov <atoporkov@gmcs.ru>
2019-02-26 11:41:56 +0300

    Annotations homework fix 2

--- annotationshomework/src/main/java/ru/annotations/test/TestLauncher.java
+++ annotationshomework/src/main/java/ru/annotations/test/TestLauncher.java
@@ -12,29 +12,45 @@ public class TestLauncher {
     public static void launch(Class cls) throws Exception {
         Object obj = cls.getDeclaredConstructor().newInstance();
         Method[] clsMeth = obj.getClass().getDeclaredMethods();
+        launchBefore(clsMeth, obj);
+        launchTest(clsMeth, obj);
+        launchAfter(clsMeth, obj);
+    }
+
+    private static void launchBefore(Method[] clsMeth, Object obj) throws Exception {
         for (Method meth : clsMeth) {
             if (meth.getAnnotation(Before.class) != null) {
                 try {
                     meth.invoke(obj);
                 } catch (IllegalAccessException ilEx) {
+                    launchAfter(clsMeth, obj);
                     throw new Exception("test preparation failed");
                 } catch (InvocationTargetException invExc) {
+                    launchAfter(clsMeth, obj);
                     throw new Exception("test preparation failed");
                 }
 
             }
         }
+    }
+
+    private static void launchTest(Method[] clsMeth, Object obj) throws Exception {
         for (Method meth : clsMeth) {
             if (meth.getAnnotation(Test.class) != null) {
                 try {
                     meth.invoke(obj);
                 } catch (IllegalAccessException ilEx) {
+                    launchAfter(clsMeth, obj);
                     throw new Exception("test run failed");
                 } catch (InvocationTargetException invExc) {
+                    launchAfter(clsMeth, obj);
                     throw new Exception("test run failed");
                 }
             }
         }
+    }
+
+    private static void launchAfter(Method[] clsMeth, Object obj) throws Exception {
         for (Method meth : clsMeth) {
             if (meth.getAnnotation(After.class) != null) {
                 try {
